28418798ac40b32659c125232aa2f1402ea4e6ff
Cameron <58471843+CameronTheDeveloper@users.noreply.github.com>
2024-03-09 18:35:30 -0800

    Add sttring methods & template literals lessons

---
+++ src/template-strings.js
@@ -0,0 +1,9 @@
+const funnyNumber = 30
+const anotherFunNumber = 50;
+
+
+const name1 = "Jacob " + funnyNumber + anotherFunNumber;
+const name2 = `Jacob ${funnyNumber} ${anotherFunNumber} ${funnyNumber + anotherFunNumber}`;
+
+console.log(name1); 
+console.log(name2);
