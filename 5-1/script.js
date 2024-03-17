217d644f8ac6e3e5c1a4bb9a7b30513fb70200e2
Cameron Posey <cameronposey@outlook.com>
2024-03-16 22:26:34 -0700

    Add 5-1 lesson - DOM manipulation

---
+++ 5-1/script.js
@@ -0,0 +1,7 @@
+const button1 = document.createElement('button');
+const container = document.querySelector('.container')
+
+button1.textContent = 'Cool Button';
+
+container.appendChild(button1);
+
