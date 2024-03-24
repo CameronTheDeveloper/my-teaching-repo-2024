974852a6e8609f93875aa6854e1a2db54f0de93b
Cameron <58471843+CameronTheDeveloper@users.noreply.github.com>
2024-03-23 23:07:21 -0700

    Add array-of-objects lesson

---
+++ src/array-of-objects.js
@@ -0,0 +1,30 @@
+const animals = [
+    {
+        name: "Giraffe",
+        legs: 4,
+        canSwim: false
+    },
+    {
+        name: "Zebra",
+        legs: 4,
+        canSwim: true
+    },
+    {
+        name: "Elephant",
+        legs: 4,
+        canSwim: true
+    }
+]
+
+console.log(animals[0])
+console.log(animals[1])
+console.log(animals[2])
+
+console.log(animals[0].name)
+console.log(animals[1].name)
+console.log(animals[2].name)
+
+
+console.log(animals[0].canSwim)
+console.log(animals[1].canSwim)
+console.log(animals[2].canSwim)
